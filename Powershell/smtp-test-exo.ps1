29e23bd3bddfce6c34d562f3536a670aa65586e6
lukasz-de <jo@lukasz.de>
2024-09-01 07:42:03 +0200

    Simple PowerShell Script to send E-Mail with SMTP

--- Powershell/smtp-test-exo.ps1
+++ Powershell/smtp-test-exo.ps1
@@ -1,4 +1,8 @@
 # Send Test Mail with Powershell
 # https://github.com/lukasz-de
 # Lukasz Hauptman | lukasz.de | 2024
+
+# Port '587' or '25' if not using TLS
+# -UseSSL or not if using non-TLS
+
 Send-MailMessage -From 'sender@yourdomain.com' -To 'recipient@yourdomain.com' -Subject "SMTP Client - $(Get-Date -Format g)" -Body "Test Microsoft 365 SMTP-Relais" -SmtpServer smtp.office365.com -Credential $cred -UseSsl -Port 587
